b02f5574b49e0fe08f4c7a45e5cfed5712b360fe
220031-Miao Heng <1098011272@qq.com>
2022-05-18 22:02:28 +0800

    before starting pa2

--- npc/playground/cpp/riscv64Top.cpp
+++ npc/playground/cpp/riscv64Top.cpp
@@ -4,44 +4,77 @@
 
 //#include "../obj_dir/Vriscv64Top__Dpi.h"
 //#include "svdpi.h"
+void single_cycle();
+void reset(int n) ;
+void sim_init();
+void sim_exit();
+void step_and_dump_wave();
 
-enum { NPC_RUNNING, NPC_STOP, NPC_END, NPC_ABORT, NPC_QUIT };
-struct NPCState{
+enum { NPC_RUNNING, NPC_END, NPC_ABORT ,NPC_QUIT/*, NPC_STOP*/ };
+typedef struct {
     int state;
     uint64_t halt_pc;
-    uint32_t halt_rer;
-} npc_state;
+    uint32_t halt_ret;
+} NPCState;
 
+NPCState npc_state;
+/*
 void ebreak() {
     npc_state.state = NPC_QUIT;
-}
+}*/
 
 VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
-
+VerilatedVcdC* tfp = NULL; 
 static Vriscv64Top  * top;
-// Combinational logic Circuit 
+
+int main() {
+    int inst[5] = {0x00100093, 0x00200113, 0x00108193,0x00100073,   };
+    // addi x1,x0, 1; addi x2, x0, 2; addi x3, x1, 1  // ebreak (used as nemu_trap)
+    int *p;
+    p = inst;
+//    int npc_quit=0;
+    sim_init();
+
+    for(int i =0; i<30; i++){
+/*        if(npc_quit==1) break;
+        switch (npc_state.state) {
+            case NPC_END: case NPC_ABORT:printf("END\n"); npc_quit=1;
+                printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+                break;
+            return 0;
+            default: npc_state.state = NPC_RUNNING;
+        }
+*/
+//        if(npc_state.state == NPC_END || npc_state.state == NPC_ABORT) break;
+
+        if(npc_state.state == NPC_RUNNING){
+            top->io_inst = *(p + i);
+            top->io_instEn =1;
+            single_cycle();
+//            npc_state.state = NPC_RUNNING;
+        } 
+        printf("%d\t%d\n",npc_state.state,i);
+
+        if(inst[i] == 0x00100073 || i == 10){break;}//npc_state.state == NPC_ABORT;}
+        //if(i == 10) {break;}//npc_state.state == NPC_END;}
+//        else npc_state.state == NPC_RUNNING;
+    }   
+    sim_exit();
+}
+
 void step_and_dump_wave(){
     top->eval();
     contextp->timeInc(1);
     tfp->dump(contextp->time());
 }
-
-// Sequential circuit 
-
-void p_cycle(){
+void single_cycle() {
     top->clock = 0; top->eval();
     contextp->timeInc(1);
     tfp->dump(contextp->time());
-}
-void n_cycle(){
+
     top->clock = 1; top->eval();
     contextp->timeInc(1);
     tfp->dump(contextp->time());
-}
-void single_cycle() {
-    p_cycle();
-    n_cycle();
 }
 void reset(int n) {
   top->reset = 1;
@@ -57,37 +90,12 @@ void sim_init(){
     top->trace(tfp, 0);
     tfp->open("../npc/playground/sim/dump.vcd");
     npc_state.state = NPC_RUNNING;
+    top->io_instEn = 1;
     reset(1);
+    printf("sim_init:%d\n",npc_state.state);
 }
 
 void sim_exit(){
     step_and_dump_wave();
     tfp->close();
-}
-int main() {
-
-    int inst[5] = {0x00100093, 0x00200113, 0x00108193, 0x00100073,   };
-    // addi x1,x0, 1; addi x2, x0, 2; addi x3, x1, 1  // ebreak (used as nemu_trap)
-    int *p;
-    int i =0;
-    p = inst;
-    sim_init();
-    top->io_instEn = 1;
-    for(; i<30; i++){
-//        if(npc_state.state == NPC_END || npc_state.state == NPC_ABORT || npc_state.state == NPC_QUIT) break;
-
-        if(npc_state.state == NPC_RUNNING){
-            top->io_inst = *(p + i);
-            single_cycle();
-            npc_state.state = NPC_RUNNING;
-            printf("%d\n",npc_state.state);
-//            break;
-            if(i==5){npc_state.state == NPC_QUIT;}
-        //} else if(npc_state.state == NPC_RUNNING && inst[i] == 0){   // NO ebreak
-          //  npc_state.state = NPC_END;
-          //  break;
-        } else if(npc_state.state == NPC_QUIT) break;
-        
-    }   
-    sim_exit();
 }
