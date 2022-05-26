3aea4d9a6ecd2e429dbe2b16e8cfe34192b27f39
220031-Miao Heng <1098011272@qq.com>
2022-05-26 10:27:59 +0800

    Achieve string.c all function, pass test; add bgeu

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,6 +14,35 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+/*
+  int sum;                                //返回值：输入字符串大小
+  char s;
+  char *ret = out;
+  va_list ap;
+
+  va_start(ap, fmt);
+
+  while (*fmt !== '\0'){
+
+    if(*fmt == '%'){
+      fmt++;                              // 检测到%，判断下一个字符
+      switch ( *fmt ){
+        case 'd':{ break;}
+        case 's':{ 
+          s = va_arg(ap, char *);
+          memcpy(out,s,strlen(s));
+          = strlen
+
+
+          break;
+          }
+        
+        default:  break; //遇到中间字符串，直接打印
+      }
+    } else {fmt++; }    //跳过之前字符串
+    
+  }
+  */
   panic("Not implemented");
 }
 
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -2,21 +2,17 @@
 #include <klib-macros.h>
 #include <stdint.h>
 //#include <debug.h>
-//#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+// test pass
 size_t strlen(const char *s) {
-  /*
   int len=0;
   while(*s++ != '\0'){
     len++;
   }
   return len;
-  */
- panic("Not implemented");
 }
-
+// test pass
 char *strcpy(char *dst, const char *src) {
 //  Assert(dst != NULL && src != NULL,"dst or src is NULL!");
   char *ret = dst;
@@ -24,27 +20,15 @@ char *strcpy(char *dst, const char *src) {
   return ret;
   //panic("Not implemented");
 }
-
+//test pass
 char *strncpy(char *dst, const char *src, size_t n) {
 //  Assert(dst != NULL && src != NULL && n > 0 , "dst or src is NULL,and n should >0!");
-/*
-    int size = sizeof(char)*(n+1);
-    char *tmp = (char *) malloc(size);
-
-    if(tmp){
-      memset(tmp, '\0', size);
-      memcpy(tmp, src, size-1);
-      memcpy(dst, tmp, size);
-
-      free(tmp);
-      return dst;
-    } else {
-      return NULL;
-    }
-    */
-  panic("Not implemented");
+  char *ret = dst;
+  while ((*dst++ =*src ++) != '\0' && n>0){n--;}
+  return ret;
+//  panic("Not implemented");
 }
-
+// test pass
 char *strcat(char *dst, const char *src) {
 //  Assert(dst != NULL && src != NULL,"dst or src is NULL!");
 
@@ -55,7 +39,7 @@ char *strcat(char *dst, const char *src) {
   
   //panic("Not implemented");
 }
-/*it is ok*/
+// test pass
 int strcmp(const char *s1, const char *s2) {
 //  Assert(s1 != NULL && s2 != NULL,"s1 or s2 is NULL!");
 
@@ -72,19 +56,19 @@ int strcmp(const char *s1, const char *s2) {
     else return -1;
   }  
 }
-
+// test pass
 int strncmp(const char *s1, const char *s2, size_t n) {
 //  Assert(s1 != NULL && s2 != NULL,"s1 or s2 is NULL!");
-  /*
+  
   while (n--){
     if(*s1 == *s2 && *s1 != '\0')
       return 0;
     else return (*s1 -*s2);
   }
-  return 0;*/
-  panic("Not implemented");
+  return 0;
+//  panic("Not implemented");
 }
-
+// test pass
 void *memset(void *s, int c, size_t n) {
 //  Assert(s != NULL ,"s is NULL!");
 
@@ -94,46 +78,43 @@ void *memset(void *s, int c, size_t n) {
   }
   return ret;
 }
-
+// test pass 
 void *memmove(void *dst, const void *src, size_t n) {
 //  Assert(dst != NULL && src != NULL,"dst or src is NULL!");
-/*
+
   void *ret = dst;
   if(dst <= src || (char *)dst >= (char *)src +n){
     while (n--){
       *(char *)dst = *(char *)src;
-      dst = (char *)dst +1;
-      src = (char *)src +1;
+      dst = ((char *)dst) +1;
+      src = ((char *)src) +1;
     }
   } else {
-    dst = (char *)dst + n -1;
-    src = (char *)src + n -1;
-  while (n--){
-    *(char *)dst = *(char *)src;
-      dst = (char *)dst -1;
-      src = (char *)src -1;
+    dst = (char *)dst + n;
+    src = (char *)src + n;
+    while (n--){
+      *(char *)dst = *(char *)src;
+        dst = (char *)dst -1;
+        src = (char *)src -1;
+    }
   }
-
-  return (ret);*/
-  panic("Not implemented");
+  return (ret);
+//  panic("Not implemented");
 }
-
+// test pass
 void *memcpy(void *out, const void *in, size_t n) {
 //  Assert(out != NULL && in != NULL,"out or in is NULL!");
-/*
-  void *ret = dst;
+  void *ret = out;
   while (n--){
     *(char *) out = * (char *) in;
     out = (char *)out + 1;
     in  = (char *)in +1;
   }
-  return (ret);*/
-  panic("Not implemented");
+  return (ret);
 }
-
+// test pass
 int memcmp(const void *s1, const void *s2, size_t n) {
 //  Assert(s1 != NULL && s2 != NULL,"s1 or s2 is NULL!");
-
   int ret =0;
   if(n == 0) return ret;
   else {
@@ -146,24 +127,6 @@ int memcmp(const void *s1, const void *s2, size_t n) {
       }
     }
   return ret;
-  /*
-  unsigned char *s1_func = (char *)s1;
-  unsigned char *s2_func = (char *)s2;
-
-    //参数判断
-    if(NULL == s1_func || NULL == s2_func || 0 > n){
-        return 0;
-    }
-
-    //比较
-    while(n-- && *(s1_func) == *(s2_func)){
-        s1_func++;
-        s2_func++;        
-    }
-
-    return ((*s1_func - *s2_func));
-    */
-//   return 0;
 }
 
 #endif
