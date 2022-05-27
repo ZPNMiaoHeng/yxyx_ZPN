41634659afd3c1df2af19b8fa2a7cb3be5071fc6
220031-Miao Heng <1098011272@qq.com>
2022-05-27 11:37:40 +0800

    Mkdir test_c and test sprintf single %s

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,10 +2,23 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+//#include <stdio.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+/*
+  va_list ap;
+  while (*fmt != '\0'){
+    if(*fmt == '%'){
+      fmt++;
+      switch(*fmt){
+        case 's':{   break;}
+      }
+    }
+  }
+  */
+//  return 13;
   panic("Not implemented");
 }
 
@@ -14,36 +27,52 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-/*
-  int sum;                                //返回值：输入字符串大小
-  char s;
-  char *ret = out;
+
+  int count = 0;                                //返回值：输入字符串大小
+  int  n;
+  char *s;
+  char buf[65];
+//  char *ret = out;
   va_list ap;
 
   va_start(ap, fmt);
 
-  while (*fmt !== '\0'){
+  while (*fmt != '\0'){
 
     if(*fmt == '%'){
       fmt++;                              // 检测到%，判断下一个字符
       switch ( *fmt ){
-        case 'd':{ break;}
-        case 's':{ 
-          s = va_arg(ap, char *);
-          memcpy(out,s,strlen(s));
-          = strlen
-
-
+        case 'd':{ 
+          n = va_arg(ap, int);
+          itoa(n,buf);
+          assert(0);
+          memmove(out,buf,strlen(buf));
+          count = strlen(buf);
+          //out++;
           break;
           }
-        
+        case 's':{ 
+          s = va_arg(ap, char *);
+          memmove(out,s,strlen(s));
+          count = strlen(s);
+          break;
+          }
+
         default:  break; //遇到中间字符串，直接打印
       }
-    } else {fmt++; }    //跳过之前字符串
-    
+    } else {
+      *out = *fmt;
+      out++;
+      if(*fmt == '\n'){}
+      
+    }    //跳过之前字符串
+    fmt++;
   }
-  */
-  panic("Not implemented");
+
+  va_end(ap);
+  return count;
+//  */
+//  panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -19,14 +19,26 @@ int abs(int x) {
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char* nptr) {                 // s -> int 
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ') { nptr ++; }          // 检测空格就,继续检测
+  while (*nptr >= '0' && *nptr <= '9') {     // 检测到字符串:字符串数字 -> 整形数字
     x = x * 10 + *nptr - '0';
     nptr ++;
   }
   return x;
+}
+
+void itoa(const int n, char* buf){
+  int i;
+  if(n < 10 ){
+    buf[0] = n + '0';
+    buf[1] = '\0';
+  } 
+  itoa(n/10, buf);
+  for (i=0; buf[i]!='\0'; i++);
+  buf[i] = (n%10) + '0';
+  buf[i+1] = '\0';
 }
 
 void *malloc(size_t size) {
@@ -40,7 +52,7 @@ void *malloc(size_t size) {
 }
 
 void free(void *ptr) {
-  
+
 }
 
 #endif
