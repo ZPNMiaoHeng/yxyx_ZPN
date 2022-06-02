a27287fae844420edf072491611da8567bf7beec
220031-Miao Heng <1098011272@qq.com>
2022-06-02 10:20:37 +0800

    Achieve iringbuf

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -9,54 +9,19 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 10
-/*
-#define BUFF_MAX_LEN 16*128
-#define VOS_OK 0
-#define VOS_ERR -1
 
-char *pHead = NULL;		   	//环形缓冲区首地址11111
-char *pValidRead = NULL;	//已使用环形缓冲区首地址
-char *pValidWrite = NULL;	//已使用环形缓冲区尾地址
-char *pTail = NULL;			  //环形缓冲区尾地址
-char readBuffer[1024];
-int readLen=0;
-
-*/
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;                                                   // ???
 
-/*
-void device_update();
-void InitRingBuff();
-void FreeRingBuff();
-int WriteRingBuff(char *pBuff, int AddLen);
-int ReadRingBuff(char *pBuff, int len);
-*/
-/* 
-static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-#ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) {
+//typedef
 
-    Log("_this->logbuf is %s\n",_this->logbuf);
-    WriteRingBuff(_this->logbuf, 128);
-    
-    readLen += ReadRingBuff(readBuffer, 256);                 // 返回值为 打印的指令条数
-    Log("ReadRingBufflen:%d\n",readLen);
+static struct iringbuf{
+  char iringp[128];
+} iringbuf[16];
 
-    if(readLen ==3){
-      ReadRingBuff(readBuffer, 128);
-      for(int i = 0; i<readLen; i++)
-              log_write("%d-->%s\n", readLen, readBuffer);
-//      log_write("%ld\n", sizeof(_this->logbuf));
-//    log_write("%s\n", _this->logbuf);                                               // itrace
-    }
-  }                            // 将本条指令中的logbuf输出到 log中
-#endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }                  // ???
-  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-}*/
+static void iRingBuf(char irp[128]);
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
@@ -96,6 +61,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 //  Log("inst information : %s\n", s->logbuf);
+//  Log("%s\n", s->logbuf);
+  iRingBuf( s->logbuf);
 #endif
 }
 
@@ -127,12 +94,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-/*改进：兼容单步执行*/
-/*
-#ifdef CONFIG_ITRACE_COND
-  InitRingBuff();
-#endif
-*/
+
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {                                                 //before execute inst nemu_state 
     case NEMU_END: case NEMU_ABORT:
@@ -159,96 +121,23 @@ void cpu_exec(uint64_t n) {
           nemu_state.halt_pc);
       // fall through
     case NEMU_QUIT: statistic();
-    /*
-#ifdef CONFIG_ITRACE_COND
-  FreeRingBuff();
-#endif*/
-
   }
 }
-/*
-void InitRingBuff(){
-  Log("--InitRingBuff--");
-	if(NULL == pHead){
-		pHead = (char *)malloc(BUFF_MAX_LEN * 128);                   //sizeof(char) = 1 分配16大小空间
-	}
 
-	memset(pHead, 0 , sizeof(BUFF_MAX_LEN)*128);                   //sizeof(16) = 4
-//    printf("%ld\t%ld\n",sizeof(char),sizeof(BUFF_MAX_LEN));
-	pValidRead = pHead;
-	pValidWrite = pHead;
-	pTail = pHead + BUFF_MAX_LEN;
+static void iRingBuf( char irp[128]){
+  static int i =0;
+  strcpy(iringbuf[i].iringp, irp);
+  if(i == 15) i = 0;
+    else i++;
+  
+  if(nemu_state.state == NEMU_ABORT || nemu_state.halt_ret ==1 ){
+    char error_flag[10] = "-->";
+    char zero_flag [10] = "   ";
+    for(int k =0; k<16; k++){
+      if(k != i-1)
+        Log("%s\t%s\n", zero_flag ,iringbuf[k].iringp);
+      else 
+        Log("%s\t%s\n", error_flag ,iringbuf[k].iringp);
+    }
+  }
 }
-
-//环形缓冲区释放
-void FreeRingBuff(){
-  Log("--FreeRingBuff--");
-	if(NULL != pHead){
-		free(pHead);
-	}
-}
-
-//向缓冲区写数据
-int WriteRingBuff(char *pBuff, int AddLen){
-  Log("pHead is %s\n", pHead);
-  Log("写入数据：pBuff is %s\n", pBuff);
-	if(NULL == pHead){
-		printf("WriteRingBuff:RingBuff is not Init!\n");
-		return VOS_ERR;
-	}
-
-	if(AddLen > pTail - pHead){
-		printf("WriteRingBuff:New add buff is too long\n");
-		return VOS_ERR;
-	}
-
-  //若新增的数据长度大于写指针和尾指针之间的长度
-	if(pValidWrite + AddLen > pTail){
-		int PreLen = pTail - pValidWrite;
-		int LastLen = AddLen - PreLen;
-		memcpy(pValidWrite, pBuff, PreLen);
-		memcpy(pHead, pBuff + PreLen, LastLen);
-
-		pValidWrite = pHead + LastLen;	//新环形缓冲区尾地址
-	}
-	else{
-    printf("-----------------依次写数据--------------------------\n");
-		memcpy(pValidWrite, pBuff, AddLen);	//将新数据内容添加到缓冲区
-		pValidWrite += 49;	//新的有效数据尾地址
-
-	}
-
-//  Log("写入数据之后：RingBuff BUFF is %s\n", pHead);
-	return VOS_OK;
-}
-
-//从缓冲区读数据
-int ReadRingBuff(char *pBuff, int len){
-	if(NULL == pHead){
-		printf("ReadRingBuff:RingBuff is not Init!\n");
-		return VOS_ERR;
-	}
-	if(len > pTail - pHead){
-		printf("ReadRingBuff:Read buff size is too long\n");
-		return VOS_ERR;
-	}
-	if(0 == len){
-		return VOS_OK;
-	}
-	if(pValidRead + len > pTail){
-		int PreLen = pTail - pValidRead;
-		int LastLen = len - PreLen;
-		memcpy(pBuff, pValidRead, PreLen);
-		memcpy(pBuff + PreLen, pHead, LastLen);
-
-		pValidRead = pHead + LastLen;
-	}
-	else{
-		memcpy(pBuff, pValidRead, len);
-    printf("pBuff is %s\n", pBuff);
-		pValidRead += len;
-	}
-
-	return 1;
-}
-*/
--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -7,6 +7,7 @@ static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
   0x0002b823,  // sd  zero,16(t0)
   0x0102b503,  // ld  a0,16(t0)
+//  0xdeadbeef,  // some data
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
