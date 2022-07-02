b3d17f546c163d0acb562644c534a89704294e6c
220031-Miao Heng <1098011272@qq.com>
2022-07-02 20:06:42 +0800

    PC有问题，准备大修改

--- npc/playground/src/main/scala/riscv64/NextPC.scala
+++ npc/playground/src/main/scala/riscv64/NextPC.scala
@@ -14,12 +14,14 @@ class NextPC extends Module {
     val NextPC = Output(UInt(64.W))
   })
 
+  val NextPC = RegInit("h8000_0000".U(64.W))
+  io.NextPC := NextPC
   val PCsrc  = Wire(UInt(2.W))
   /* 通过译码得到Branch信号 -----> PCsrc信号（控制PC与 4/imm 操作） */
   PCsrc := MuxCase("b00".U, Array(                                                      // 默认：PC + 4
     (io.Branch === "b001".U) -> "b10".U,                                                // 无条件跳转PC目标: PC + imm
     (io.Branch === "b010".U) -> "b11".U                                                 // 无条件跳转寄存器目标: rs1 + imm 
   ))
-  io.NextPC := Mux(PCsrc(1) === 0.U, 4.U, io.imm) + Mux(PCsrc(0) === 0.U, io.PC, io.rs1)
-
+  NextPC := Mux(PCsrc(1) === 0.U, 4.U, io.imm) + Mux(PCsrc(0) === 0.U, io.PC, io.rs1)
+  
 }
