ea0be523a890126842b4e7bd179b2a1a2752f7a0
220031-Miao Heng <1098011272@qq.com>
2022-07-17 23:09:49 +0800

    CLock is error

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -10,8 +10,10 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 4);
+  assert(offset == 0 || offset == 4); // Only read two datas
+  printf("rtc_to_handler\n");
   if (!is_write && offset == 4) {
+    printf("in if\n");
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
