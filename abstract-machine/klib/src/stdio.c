e3ee3de67db72c2c52d301f603697d0e767c3f61
220031-Miao Heng <1098011272@qq.com>
2022-07-17 13:55:17 +0800

    printf->%S/%D/%X/%c ,# with '#' will be ignored, and an empty message aborts the commit.

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,39 +5,76 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char buffer [32];
 void itoa(const int n, char* buf);
-static int mini_itoa(long value, unsigned int radix, int uppercase, int unsig,char *buffer);
-static int mini_pad(char* ptr, int len, char pad_char, int pad_to, char *buffer);
+
+static int
+mini_itoa(long value, unsigned int radix, int uppercase, int unsig,char *buffer) {
+  /* radix->, */
+  char  *pbuffer = buffer;
+  int negative = 0;
+  int i, len;
+
+  /* No support for unusual radixes. */
+  if (radix > 16) return 0;
+
+  if (value < 0 && !unsig) {
+    negative = 1;
+    value = -value;
+  }
+
+  /* This builds the string back to front ... */
+  do {
+    int digit = value % radix;
+    *(pbuffer++) = (digit < 10 ? '0' + digit : (uppercase ? 'A' : 'a') + digit - 10);
+    value /= radix;
+  } while (value > 0);
+
+  if (negative)
+    *(pbuffer++) = '-';
+
+  *(pbuffer) = '\0';
+
+  /* ... now we reverse it (could do it recursively but will
+   * conserve the stack space) */
+  len = (pbuffer - buffer);
+  for (i = 0; i < len / 2; i++) {
+    char j = buffer[i];
+    buffer[i] = buffer[len-i-1];
+    buffer[len-i-1] = j;
+  }
+
+  return len;
+}
+
+void itoa(const int n, char* buf){
+  int i;
+  if(n < 10 ){
+    buf[0] = n + '0';
+    buf[1] = '\0';
+  } else {
+    itoa(n/10, buf);
+    for (i=0; buf[i]!='\0'; i++);
+    buf[i] = (n%10) + '0';
+    buf[i+1] = '\0';
+  }
+}
+
 
 int printf(const char *fmt, ...) {
 
   static int count = 0 ,n ;
-  char buf[24];
-  char buf2[24];
+  char buf[65];
   char *s=buf;
   char ch;
   memset(buf, 0, sizeof(buf));
   va_list ap;
 	va_start(ap, fmt);
   
-  while ((ch = *(fmt++))) {
+	while (*fmt != '\0'){
     if (*fmt == '%'){
-      char pad_char = ' ';
-			int pad_to = 0;
-			char *ptr;
-			ch=*(fmt++);
-			/* Zero padding requested */
-			if (ch == '0') pad_char = '0';
-			/* Data Width */
-      while (ch >= '0' && ch <= '9') {
-				pad_to = pad_to * 10 + (ch - '0');
-				ch=*(fmt++);
-			}
-			if(pad_to > (signed int) sizeof(buf)) {
-				pad_to = sizeof(buf);
-			}
-
-			switch (ch){
+      
+			switch (*++fmt){
         case 'c': {
           putch('C');
           ch = (char)(va_arg(ap, int));
@@ -157,80 +194,3 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 
 #endif
 
-static int
-mini_itoa(long value, unsigned int radix, int uppercase, int unsig,char *buffer) {
-  /* radix->, */
-  char  *pbuffer = buffer;
-  int negative = 0;
-  int i, len;
-
-  /* No support for unusual radixes. */
-  if (radix > 16) return 0;
-
-  if (value < 0 && !unsig) {
-    negative = 1;
-    value = -value;
-  }
-
-  /* This builds the string back to front ... */
-  do {
-    int digit = value % radix;
-    *(pbuffer++) = (digit < 10 ? '0' + digit : (uppercase ? 'A' : 'a') + digit - 10);
-    value /= radix;
-  } while (value > 0);
-
-  if (negative)
-    *(pbuffer++) = '-';
-
-  *(pbuffer) = '\0';
-
-  /* ... now we reverse it (could do it recursively but will
-   * conserve the stack space) */
-  len = (pbuffer - buffer);
-  for (i = 0; i < len / 2; i++) {
-    char j = buffer[i];
-    buffer[i] = buffer[len-i-1];
-    buffer[len-i-1] = j;
-  }
-
-  return len;
-}
-
-void itoa(const int n, char* buf){
-  int i;
-  if(n < 10 ){
-    buf[0] = n + '0';
-    buf[1] = '\0';
-  } else {
-    itoa(n/10, buf);
-    for (i=0; buf[i]!='\0'; i++);
-    buf[i] = (n%10) + '0';
-    buf[i+1] = '\0';
-  }
-}
-
-static int
-mini_pad(char* ptr, int len, char pad_char, int pad_to, char *buffer)
-{
-	int i;
-	int overflow = 0;
-	char * pbuffer = buffer;
-	if(pad_to == 0) pad_to = len;
-	if(len > pad_to) {
-		len = pad_to;
-		overflow = 1;
-	}
-	for(i = pad_to - len; i > 0; i --) {
-		*(pbuffer++) = pad_char;
-	}
-	for(i = len; i > 0; i --) {
-		*(pbuffer++) = *(ptr++);
-	}
-	len = pbuffer - buffer;
-	if(overflow) {
-		for (i = 0; i < 3 && pbuffer > buffer; i ++) {
-			*(pbuffer-- - 1) = '*';
-		}
-	}
-	return len;
-}
